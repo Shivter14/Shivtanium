0b6f033f84cd3751123c67517001f1ce1e749ee2
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-05-12 11:27:53 -0700

    Update boot_FAF.bat
    Made fade-out faster

--- C/Shivtanium/boot_FAF.bat
+++ C/Shivtanium/boot_FAF.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 for /f "tokens=1-4 delims=:.," %%a in ("!time: =0!") do set /a "t1=((((1%%a-1000)*60+(1%%b-1000))*60+(1%%c-1000))*100)+(1%%d-1000),t2=t1"
 set fadeout=255
 for /l %%. in () do (
-	for /f "tokens=1-4 delims=:.," %%a in ("!time: =0!") do set /a "t1=((((1%%a-1000)*60+(1%%b-1000))*60+(1%%c-1000))*100)+(1%%d-1000)", "DeltaTime=(t1-t2)", "t2=t1", "fadeout-=deltaTime*400/fadeout"
+	for /f "tokens=1-4 delims=:.," %%a in ("!time: =0!") do set /a "t1=((((1%%a-1000)*60+(1%%b-1000))*60+(1%%c-1000))*100)+(1%%d-1000)", "DeltaTime=(t1-t2)", "t2=t1", "fadeout-=deltaTime*800/fadeout"
 	if "!deltaTime!" neq "0" (
 		set /p "=%\e%[38;2;!fadeout!;!fadeout!;!fadeout!;48;2;;;m%\e%[!sst.boot.logoY!;!sst.boot.logoX!H!spr.[bootlogo.spr]!%\e%[!sst.boot.msgY!;!sst.boot.msgX!H%\e%[2K!sst.boot.msg!"
 	) < nul
