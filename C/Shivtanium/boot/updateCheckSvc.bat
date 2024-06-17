d646073750ea9728d475f1441b80df0557fc56c6
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-06-17 09:58:16 -0700

    Update updateCheckSvc.bat

--- C/Shivtanium/boot/updateCheckSvc.bat
+++ C/Shivtanium/boot/updateCheckSvc.bat
@@ -4,6 +4,6 @@ if not exist "!sst.dir!\temp" (
 	echo This program is a Shivtanium service and cannot be called outside of Shivtanium.
 	exit /b 1
 )
-REM Under construction
+REM Still under construction muhehehe
 copy nul "!sst.dir!\temp\pf-%~n0" > nul
 exit /b
