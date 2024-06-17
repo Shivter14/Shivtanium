f675a6285c777e1cc17dadb945f9f8192153195d
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-06-17 09:58:46 -0700

    Update ic-shutdown.bat

--- C/Shivtanium/core/ic-shutdown.bat
+++ C/Shivtanium/core/ic-shutdown.bat
@@ -1,8 +1,10 @@
-if exist "!asset[\sounds\shutdown.mp3]!" start /b cscript.exe //b core\playsound.vbs "!asset[\sounds\shutdown.mp3]!"
-for %%a in (!windows!) do echo(¤DW	%%a
-rem echo(¤OV	%\e%[H%\e%[48;2;;;;38;2;255;255;255m%\e%[2JSystem is shutting down. . .
 set /a "x=(sys.modeW-48)/2, y=(sys.modeH-5)/2"
-echo(¤CW	sys.shutdown	!x!	!y!	48	5	System Shutdown
-echo(¤MW	sys.shutdown	l2=  Shivtanium is shutting down. . .
-echo(¤EXIT
-exit 27
+for %%a in (!windows!) do echo=¤DW	%%a
+echo=¤OV
+echo=¤CW	sys.shutdown	!x!	!y!	48	5	System Shutdown
+echo=¤MW	sys.shutdown	l2=  Shivtanium is shutting down. . .
+echo=¤CTRL	ORDER	sys.shutdown
+echo=¤EXIT
+rem if exist "!asset[\sounds\shutdown.mp3]!" start /b cscript.exe //b core\playsound.vbs "!asset[\sounds\shutdown.mp3]!"
+title Shivtanium !sys.tag! !sys.ver! !sys.subvinfo! ^| Shutting down. . .
+exit 0
