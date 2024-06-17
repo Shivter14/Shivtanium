6e26f4f28316a9861b1953a17eb6ad0ee70fdf8c
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-06-17 09:59:21 -0700

    Update ic-WinAPI.bat
    my lazy ass actually added text inputs now

--- C/Shivtanium/core/ic-WinAPI.bat
+++ C/Shivtanium/core/ic-WinAPI.bat
@@ -1,7 +1,15 @@
 set "parameters=%~2"
-for /f "tokens=1* delims=	" %%0 in ("!parameters!") do (
+for /f "tokens=1-2* delims=	" %%0 in ("!parameters!") do (
 	if /I "%%~0"=="setTextInput" (
-		if "!win[%~1.%%~1]!"=="" call Shivtanium.bat :haltIC "%~n0" %1 "Attempt to modify non-existent window" "Window ID: %%~1\nAction: setTextInput"
+		if "!win[%~1.%%~1]!"=="" (
+			call Shivtanium.bat :haltIC "%~n0" %1 "Attempt to modify non-existent window" "Window ID: %%~1\nAction: setTextInput"
+			exit /b 1
+		)
+		if "!win[%~1.%%~1]txt[%%~2]!" neq "" (
+			call Shivtanium.bat :haltIC "%~n0" %1 "Attempt to overwrite text input" "Window ID: %%~1\nAction: setTextInput\nText input variable: %%~2"
+			exit /b 1
+		)
+		if "!win.focused!"=="%~1.%%~1" set "txtIn.focused=%1.%%~2"
 	) else set "pid[%~1]vreturn=Unknown command"
 )
 set parameters=
