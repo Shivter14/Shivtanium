de11ab2222f53daf9dcdd9cf58bfb2e4b12f7c62
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-06-19 21:19:37 +0200

    Update autorun.cmd

--- C/autorun.cmd
+++ C/autorun.cmd
@@ -1,6 +1,5 @@
-@echo off
-setlocal enabledelayedexpansion
-chcp 65001>nul
+@echo off & setlocal enabledelayedexpansion
+chcp 65001 > nul
 for /f %%a in ('echo prompt $E^| cmd') do set "\e=%%a"
 for /f "tokens=2 delims=:" %%a in ('mode con') do (
 	set /a counter+=1
@@ -10,5 +9,5 @@ for /f "tokens=2 delims=:" %%a in ('mode con') do (
 )
 set /a "x=(modeW-56)/2, y=(modeH-4)/2"
 echo %\e%[!y!;!x!H█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█%\e%[!x!G%\e%[B█ This program requires to be run in SSVM 3 or later.  █%\e%[!x!G%\e%[B█ If you want to run it without SSVM, run autorun.bat. █%\e%[!x!G%\e%[B█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
-pause<con>nul
+pause < con > nul
 exit /b 1
