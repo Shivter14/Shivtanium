61afb3015b183bc6ff2facdfb88f3a259b768bef
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-07-20 23:30:07 +0200

    Update sstoskrnl.bat

--- C/Shivtanium/sstoskrnl.bat
+++ C/Shivtanium/sstoskrnl.bat
@@ -7,7 +7,9 @@ if !sys.CPU.count! lss 4 (
 	call :createProcess 0	systemb-dialog.bat 5 3 59 9 "Performance notification	noCBUI" "l2=  Your CPU only has !sys.CPU.count! logical processor(s).	l3=  This might lead to a bad experience.	l4=  The recommended minimum of logical processors is: 4	!temp.nr!" 50 7 7 " Close "
 	set temp.nr=
 )
-call :createProcess 0	systemb-login.bat
+if not exist "!sst.root!\Users" (
+	call :createProcess 0	systemb-oobe.bat
+) else call :createProcess 0	systemb-login.bat
 set sys.keys=
 set keysPressedOld=
 set "windows= "
