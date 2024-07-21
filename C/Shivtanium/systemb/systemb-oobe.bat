ccb05ebb5ce15738e85409a35aad7e4e16d5e0af
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-07-21 16:28:58 +0200

    Update systemb-oobe.bat
    Did some progress with the Font installing

--- C/Shivtanium/systemb/systemb-oobe.bat
+++ C/Shivtanium/systemb/systemb-oobe.bat
@@ -90,11 +90,11 @@ set "txt.password= "
 call :textinput username
 call :pagewait
 
-for /l %%a in (!inputW! -3 4) do (
+for /l %%a in (!inputW! -4 4) do (
 	set /a "x=inputW-%%a+3"
 	echo=¤MW	!PID!.oobe^
 	l2=%\e%[!x!C!l2:~0,%%a!	l4=%\e%[!x!C!l4:~0,%%a!	l7=%\e%[!x!C!l7:~0,%%a!^
-	o5=%\e%[!x!C%\e%[%%aX!txt.username!	o8=%\e%[!x!C%\e%[%%aX!txt.password!
+	o5=%\e%[!x!C%\e%[%%aX!txt.username:~0,%%a!	o8=%\e%[!x!C%\e%[%%aX!txt.password:~0,%%a!
 )
 set x=
 echo=¤MW	!PID!.oobe	o5=	o8=	l2=	l4=	l7=
@@ -105,13 +105,23 @@ set "l2=Font installation                                        "
 set "l4=The recommended font for Shivtanium is:                  "
 set "l5= MxPlus IBM VGA 8x16                                     "
 set "l6=This will download & extract a font pack from int10h.org."
-set "l8=Do you want to download & install this font؟             "
-for /l %%a in (1 3 56) do echo=¤MW	!PID!.oobe^
-	l2=  !l2:~-%%a!	l4=  !l4:~-%%a!	l5=  %\e%[7m!l5:~-%%a,21!%\e%[27m	l6=  !l6:~-%%a!	l8=  !l8:~-%%a!
-echo=¤MW	!PID!.oobe	l2=  !l2:  =!	l4=  !l4:  =!	l5=  %\e%[7m!l5:~0,21!%\e%[27m	l6=  !l6:  =!	l8=  !l8:  =!
+set "l8=If you want to skip this font installation, click 'Next'."
+set "o10= Download & Install                                     "
+set buttons=install
+set btn[install]X=3
+set btn[install]BX=22
+set btn[install]Y=10
+set btn[install]=:getfonts
+set "btn[install]title=!o10:~0,20!"
+set continue=
 
+for /l %%a in (2 4 56) do echo=¤MW	!PID!.oobe^
+	l2=  !l2:~-%%a!	l4=  !l4:~-%%a!	l5=  %\e%[7m!l5:~-%%a,21!%\e%[27m	l6=  !l6:~-%%a!	l8=  !l8:~-%%a!	o10=%\e%[3C!o10:~-%%a,20!
+echo=¤MW	!PID!.oobe	l2=  !l2:  =!	l4=  !l4:  =!	l5=  %\e%[7m!l5:~0,21!%\e%[27m	l6=  !l6:  =!	l8=  !l8:  =!	o10=%\e%[3C!o10:~0,20!
 
 call :pagewait
+echo=¤MW	!PID!.oobe	l2=	l4=	l5=	l6=	l8=	o10=
+if errorlevel 1 goto accountsetup
 goto start
 :pagewait
 for /l %%# in (1 1 1000) do if not defined continue for /l %%# in (1 1 1000) do if not defined continue (
@@ -153,8 +163,10 @@ for /l %%# in (1 1 1000) do if not defined continue for /l %%# in (1 1 1000) do 
 	) else set "!kernelOut!">nul 2>&1
 )
 if not defined continue goto pagewait
-set continue=
-exit /b !continue!
+for /f "delims=" %%a in ("!continue!") do (
+	set continue=
+	exit /b %%a
+)
 :textinput
 echo=¤MW	!PID!.oobe	o!btn[%1]Y!=%\e%[3C%\e%[7m%\e%[!inputW!X!txt.%1:~-%inputC%!_%\e%[27m
 set continue=
@@ -183,6 +195,7 @@ for /l %%# in (1 1 1000) do if not defined continue for /l %%# in (1 1 1000) do 
 					set "txt.%~1=!txt.%~1!!char!"
 					echo=¤MW	!PID!.oobe	o!btn[%1]Y!=%\e%[3C%\e%[7m%\e%[!inputW!X!txt.%1:~-%inputC%!_%\e%[27m
 				) else if "%%~k"=="13" (
+					set continue.exit=0
 					set continue=13
 					echo=¤MW	!PID!.oobe	o!btn[%1]Y!=%\e%[3C%\e%[!inputW!X!txt.%1:~-%inputC%!
 				)
@@ -202,7 +215,7 @@ for /l %%# in (1 1 1000) do if not defined continue for /l %%# in (1 1 1000) do 
 		)
 	) else if "!kernelOut!"=="click=0" (
 		if "!focusedWindow!"=="!PID!.oobe" (
-			echo=¤MW	!PID!.oobe	o!contentH!=%\e%[!nextButtonX!C Next 
+			echo=¤MW	!PID!.oobe	o!contentH!=%\e%[2C Back %\e%8%\e%[!nextButtonX!C Next 
 			set /a "relativeMouseX=mouseXpos - win[!PID!.oobe]X, relativeMouseY=mouseYpos - win[!PID!.oobe]Y"
 			if "!relativeMouseY!" neq "!btn[%~1]Y!" (
 				set continue=True
@@ -223,6 +236,86 @@ if not defined continue goto textinput.loop
 if "!continue!"=="13" (
 	if defined btn[%1]N (
 		call !btn[%1]N!
+		set continue=0
 	)
 ) else set continue=!continue.exit!
+exit /b 0
+:getfonts
+echo=¤MW	!PID!.oobe	o10=%\e%[3C%\e%[7m!btn[install]title!%\e%[27m
+set continue=
+for /l %%# in (1 1 1000) do if not defined continue for /l %%# in (1 1 1000) do if not defined continue (
+	set kernelOut=
+	set /p "kernelOut="
+	if defined kernelOut (
+		if "!kernelOut!"=="click=0" (
+			set continue=True
+			if "!focusedWindow!"=="!PID!.oobe" set /a "relativeMouseX=mouseXpos - win[!PID!.oobe]X, relativeMouseY=mouseYpos - win[!PID!.oobe]Y, click=0"
+		) else if "!kernelOut!"=="exitProcess=!PID!" (
+			exit 0
+		) else if "!kernelOut!"=="exit" (
+			exit 0
+		) else set "!kernelOut!">nul 2>&1
+	)
+)
+if not defined continue goto getfonts
+set continue=
+echo=¤MW	!PID!.oobe	o10=%\e%[3C!btn[install]title!
+if "!click!" neq "0" exit /b 0
+if "!relativeMouseY!" neq "!btn[install]Y!" exit /b 0
+if !relativeMouseX! lss !btn[install]X! exit /b 0
+if !relativeMouseX! gtr !btn[Install]BX! exit /b 0
+
+md "!sst.dir!\temp\proc\PID-!PID!-dir" || exit /b 1
+pushd "!sst.dir!\temp\proc\PID-!PID!-dir" || exit /b 1
+
+echo=¤CW	!PID!.getfonts	5	3	84	8	Downloading assets	classic noCBUI
+>>"!sst.dir!\temp\kernelPipe" echo=registerWindow	!PID!	!PID!.getfonts	5	3	84	8
+set contentW=80
+echo=¤MW	!PID!.getfonts	l2=  Downloading %\e%[7m oldschool_pc_font_pack_v2.2_win.zip %\e%[27m . . .	l4=  %% Total    %% Received %% Xferd  Average Speed   Time    Time     Time  Current	l5=                                 Dload  Upload   Total   Spent    Left  Speed
+
+curl -fkLO https://int10h.org/oldschool-pc-fonts/download/oldschool_pc_font_pack_v2.2_win.zip 2>&1 | call "!sst.dir!\core\streamIntoWindow" !PID!.getfonts	l6=  
+if errorlevel 1 (
+	call :getfonts.fail "Failed to download assets" "The following file failed to download:" "%\e%[7m oldschool_pc_font_pack_v2.2_win.zip %\e%[27m" "Reason: %\e%[7m curl error !errorlevel! %\e%[27m"
+	goto getfonts.exit
+) else if not exist "oldschool_pc_font_pack_v2.2_win.zip" (
+	call :getfonts.fail "Failed to download assets" "The following file failed to download:" "%\e%[7m oldschool_pc_font_pack_v2.2_win.zip %\e%[27m" "Reason: Generic error !errorlevel!"
+	goto getfonts.exit
+)
+echo=¤MW	!PID!.getfonts	l2=  Extracting %\e%[7m oldschool_pc_font_pack_v2.2_win.zip %\e%[27m . . .	l4=	l5=	l6=
+tar -xf "oldschool_pc_font_pack_v2.2_win.zip" || (
+	call :getfonts.fail "Failed to extract assets" "The following archive failed to extract:" "%\e%[7m oldschool_pc_font_pack_v2.2_win.zip %\e%[27m" "Reason: %\e%[7m tar error !errorlevel! %\e%[27m"
+	goto getfonts.exit
+)
+
+popd
+rd /s /q "!sst.dir!\temp\proc\PID-!PID!-dir" >&2
+:getfonts.exit
+echo=¤DW	!PID!.getfonts
+>>"!sst.dir!\temp\kernelPipe" echo=unRegisterWindow	!PID!.getfonts
+exit /b 0
+:getfonts.fail
+set halt.title=
+set halt.winparams=
+set halt.winW=16
+set lines=1
+for %%a in (%*) do (
+	set "msg=#%%~a"
+	set "msglen=0"
+	for /l %%b in (9,-1,0) do (
+		set /a "msglen|=1<<%%b"
+		for %%c in (!msglen!) do if "!msg:~%%c,1!" equ "" set /a "msglen&=~1<<%%b"
+	)
+	
+	if not defined halt.title (
+		set /a msglen+=13
+		set "halt.title=%%~a"
+	) else (
+		set /a "msglen+=6, lines+=1"
+		set "halt.winparams=!halt.winparams!	l!lines!=  %%~a"
+	)
+	if !msglen! gtr !halt.winW! set "halt.winW=!msglen!"
+)
+set /a "halt.winH=lines + 4"
+
+>>"!sst.dir!\temp\kernelPipe" echo=createProcess	!PID!	systemb-dialog 5 3 !halt.winW! !halt.winH! "!halt.title!	classic noCBUI" "!halt.winparams!" w-buttonW-2 h-2 7 " Close "
 exit /b 0
