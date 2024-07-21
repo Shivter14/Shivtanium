b6ae060dd23780d76831f04aa799770b40b3c9ac
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-07-21 22:38:54 +0200

    Update updateCheckSvc.bat

--- C/Shivtanium/boot/updateCheckSvc.bat
+++ C/Shivtanium/boot/updateCheckSvc.bat
@@ -1,9 +1,32 @@
-@echo off
-setlocal enableDelayedExpansion
+@echo off & setlocal enableDelayedExpansion
 if not exist "!sst.dir!\temp" (
 	echo This program is a Shivtanium service and cannot be called outside of Shivtanium.
 	exit /b 1
 )
-REM Still under construction muhehehe
+
+if /I "!sys.boot.disableUpdateChecks!" neq "True" (
+	set latest.tag=
+	set latest.ver=
+	set latest.subvinfo=
+	for /f "delims=*^!" %%a in ('curl -sm 3 "https://github.com/Shivter14/Shivtanium/releases/download/latest/update.dat" 2^>^&1') do (
+		set "str=%%a"
+			if "!str:~0,4!"=="tag=" set "latest.tag=!str:~4!"
+		if "!str:~0,4!"=="ver=" set "latest.ver=!str:~4!"
+		if "!str:~0,9!"=="subvinfo=" set "latest.ver=!str:~9!"
+	)
+	if defined latest.tag if "!latest.tag!" neq "!sys.tag!" goto updateFound
+	if defined latest.ver if "!latest.ver!" neq "!sys.ver!" goto updateFound
+	if defined latest.subvinfo if "!latest.subvinfo!" neq "!sys.subvinfo!" goto updateFound
+)
+
 copy nul "!sst.dir!\temp\pf-%~n0" > nul
-exit /b
+exit 0
+:updateFound
+(
+	echo=!latest.tag!
+	echo=!latest.ver!
+	echo=!latest.subvinfo!
+) > "!sst.dir!\temp\update.dat"
+>>"!sst.dir!\temp\kernelPipe" echo=createProcess	0	boot\update.bat
+copy nul "!sst.dir!\temp\pf-%~n0" > nul
+exit 0
