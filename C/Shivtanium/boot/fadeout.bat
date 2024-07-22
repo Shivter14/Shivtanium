6767fef64f63139ab06d030a4ba335dcb6019a4c
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-07-22 22:43:44 +0200

    Update fadeout.bat

--- C/Shivtanium/boot/fadeout.bat
+++ C/Shivtanium/boot/fadeout.bat
@@ -12,7 +12,7 @@ del "!sst.dir!\temp\bootStatus-!sst.localtemp!" > nul 2>&1
 del "!sst.dir!\temp\bootStatus-!sst.localtemp!-exit" > nul 2>&1
 set /a sst.boot.fadeout+=1
 for /l %%. in () do (
-	for /f "tokens=1-4 delims=:.," %%a in ("!time: =0!") do set /a "t1=((((1%%a-1000)*60+(1%%b-1000))*60+(1%%c-1000))*100)+(1%%d-1000)", "DeltaTime=(t1-t2)", "t2=t1", "sst.boot.fadeout-=deltaTime*800/sst.boot.fadeout"
+	for /f "tokens=1-4 delims=:.," %%a in ("!time: =0!") do set /a "t1=((((1%%a-1000)*60+(1%%b-1000))*60+(1%%c-1000))*100)+(1%%d-1000)", "DeltaTime=(t1-t2)", "t2=t1", "sst.boot.fadeout-=deltaTime*500/sst.boot.fadeout"
 	if "!deltaTime!" neq "0" (
 		echo=%\e%[38;2;!sst.boot.fadeout!;!sst.boot.fadeout!;!sst.boot.fadeout!;48;2;;;m%\e%[!sst.boot.logoY!;!sst.boot.logoX!H!spr.[bootlogo.spr]!%\e%[!sst.boot.msgY!;!sst.boot.msgX!H%\e%[2K!sst.boot.msg!
 	) < nul
