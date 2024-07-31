757d81e3795b5df8451dd9314e9cf0bf0e0be952
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-07-31 14:31:16 -0700

    Update preparePipe.bat

--- C/Shivtanium/preparePipe.bat
+++ C/Shivtanium/preparePipe.bat
@@ -1,6 +1,5 @@
 @echo off
-for /f "tokens=1 delims==" %%a in ('set pid[ 2^>nul') do set "%%a="
-for /f "tokens=1 delims==" %%a in ('set win[ 2^>nul') do set "%%a="
+for /f "tokens=1 delims==" %%a in ('set pid[ 2^>nul ^& set win[ 2^>nul') do set "%%a="
 for /l %%a in (1 1 %ioTotal%) do set /p "="
 call %*
 set /a PID=PID
