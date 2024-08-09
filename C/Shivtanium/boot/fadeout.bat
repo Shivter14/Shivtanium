4331c2eded4d77205811b9dc227a2f92482ff793
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-08-09 23:42:05 +0200

    Update fadeout.bat

--- C/Shivtanium/boot/fadeout.bat
+++ C/Shivtanium/boot/fadeout.bat
@@ -8,7 +8,7 @@ set /a wait+=1
 if !wait! lss 10000 if not exist "!sst.dir!\temp\bootStatus-!sst.localtemp!-exit" goto autisticLoop
 del "!sst.dir!\temp\bootStatus-!sst.localtemp!" > nul 2>&1
 del "!sst.dir!\temp\bootStatus-!sst.localtemp!-exit" > nul 2>&1
-set /a sst.boot.fadeout+=1
+set sst.boot.fadeout=255
 for /f "tokens=1-4 delims=:.," %%a in ("!time: =0!") do set /a "t1=((((1%%a-1000)*60+(1%%b-1000))*60+(1%%c-1000))*100)+(1%%d-1000),t2=t1"
 for /l %%. in () do (
 	for /f "tokens=1-4 delims=:.," %%a in ("!time: =0!") do set /a "t1=((((1%%a-1000)*60+(1%%b-1000))*60+(1%%c-1000))*100)+(1%%d-1000)", "DeltaTime=(t1-t2)", "t2=t1", "sst.boot.fadeout-=deltaTime*500/sst.boot.fadeout"
