ae4bf469880187eb8579155563dd9f0227f7f3a7
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-09-25 11:08:52 -0700

    Update and rename update.bat to update.bxf
    Currently working on updating

--- C/Shivtanium/boot/update.bat
+++
@@ -1,8 +0,0 @@
-@echo off & setlocal enableDelayedExpansion
-if not defined PID (
-	echo=This program is a Shivtanium startup service.
-	<nul set /p "=Press any key to exit. . ."
-	pause > nul
-	exit /b 1
-)
-set /a PID=PID
