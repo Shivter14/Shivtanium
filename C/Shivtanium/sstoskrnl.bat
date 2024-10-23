4cb852bbb453697a722407b3f6850065bb18dfe6
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-10-23 11:26:41 -0700

    Update sstoskrnl.bat
    - Windows can now be moved by holding the Windows/Super key and dragging a window from any point.
    - Added new system variable: `autorun` (list)
      This variable can contain processes that will be started on boot.
      Format: `"<PID of parent> <command> [<parameters>]"`
      Example: `"0 programs\ExampleService\svc.bat" "0 systemb-textview test.txt"`

--- C/Shivtanium/sstoskrnl.bat
+++ C/Shivtanium/sstoskrnl.bat
@@ -19,8 +19,9 @@ if /I "%~1"=="/forceoobe" (
 	call :createProcess 0	systemb-oobe.bat
 ) else (
 	if exist "!asset[\sounds\boot.mp3]!" start "<Shivtanium startup sound handeler> (ignore this)" /min cscript.exe //b core\playsound.vbs "!asset[\sounds\boot.mp3]!"
-	call :createProcess 0	systemb-login.bat
+	for %%a in (!sst.autorun!) do call :createProcess %%~a
 )
+set sst.autorun=
 set sys.keys=
 set keysPressedOld=
 set "windows= "
@@ -28,94 +29,10 @@ set "windowsT= "
 set ioTotal=0
 title Shivtanium OS !sys.tag! !sys.ver! !sys.subvinfo!
 
-
+set "clamp=(leq=((low-x)>>31)+1)*low+(geq=((x-high)>>31)+1)*high+^^^!(leq+geq)*x"
+(
+set clamp=
 for /l %%# in () do (
-	set "sys.clickOld=!sys.click!"
-	set "sys.click=!click!"
-	if "!sys.click!" neq "!sys.clickOld!" (
-		set sys.mouseXpos=!mouseXpos!
-		set sys.mouseYpos=!mouseYpos!
-		if !sys.mouseXpos! geq 1 if !sys.mouseXpos! leq !sys.modeW! if !sys.mouseYpos! geq 1 if !sys.mouseYpos! leq !sys.modeH! (
-			if "!sys.click!"=="1" (
-				set "_focusedWindow=!focusedWindow!"
-				set focusedWindow=
-				for %%w in (!windows!) do if not defined focusedWindow (
-					if !sys.mouseYpos! geq !win[%%~w]Y! if !sys.mouseYpos! leq !win[%%~w]BY! if !sys.mouseXpos! geq !win[%%~w]X! if !sys.mouseXpos! leq !win[%%~w]BX! (
-						if "%%~w" neq "!_focusedWindow!" (
-							set "focusedWindow=%%~w"
-							set "windows= %%w!windows: %%w = !"
-							echo=focusedWindow=%%~w
-							>&3 echo=¤FOCUS	%%~w
-							set /a ioTotal+=1
-						) else set "focusedWindow=%%~w"
-						if "!sys.mouseYpos!"=="!win[%%~w]Y!" if "!win[%%~w]D:~0,1!" neq "1" (
-							set /a "movingWindowOffset=sys.mouseXpos-!win[%%~w]X!, tempX=!win[%%~w]X!, tempY=!win[%%~w]Y!"
-							set "movingWindow=!focusedWindow!"
-						)
-					)
-				)
-				if defined _focusedWindow (
-					if not defined focusedWindow (
-						echo=focusedWindow=
-						set /a ioTotal+=4
-					) else set /a ioTotal+=3
-				) else set /a ioTotal+=3
-			) else (
-				if defined movingWindow (
-					set /a ioTotal+=5
-					if not defined tempX call :kernelPanic "Fatal error in kernel-side window manager" "Attempted to move a window to an undefined position. This is a critical bug, please report it at: github.com/Shivter14/Shivtanium/issues"
-					echo=win[!movingWindow!]X=!tempX!
-					echo=win[!movingWindow!]Y=!tempY!
-					set movingWindow=
-				) else (
-					set movingWindow=
-					set /a ioTotal+=3
-				)
-			)
-			echo=mouseXpos=!sys.mouseXpos!
-			echo=mouseYpos=!sys.mouseYpos!
-			echo=click=!sys.click!
-		)
-	)
-	set sys.mouseXold=!sys.mouseXpos!
-	set sys.mouseYold=!sys.mouseYpos!
-	if "!random:~0,1!"=="1" (
-		set sys.mouseXpos=!mouseXpos!
-		set sys.mouseYpos=!mouseYpos!
-		if "!sys.mouseXold!;!sys.mouseYold!" neq "!sys.mouseXpos!;!sys.mouseYpos!" if defined movingWindow (
-			set /a "tempX=win[!movingWindow!]X=sys.mouseXpos-movingWindowOffset, temps=win[!movingWindow!]BX=win[!movingWindow!]X+win[!movingWindow!]W-1", "tempY=win[!movingWindow!]Y=sys.mouseYpos, win[!movingWindow!]BY=win[!movingWindow!]Y+win[!movingWindow!]H-1"
-			if !tempX! lss 1 (
-				set /a "tempX=win[!movingWindow!]X=1, win[!movingWindow!]BX=win[!movingWindow!]W"
-			) else if !temps! gtr !sys.modeW! set /a "tempX=win[!movingWindow!]X=sys.modeW-win[!movingWindow!]W+1, win[!movingWindow!]BX=sys.modeW"
-			if !tempY! lss 1 (
-				set /a "tempY=win[!movingWindow!]Y=1, win[!movingWindow!]BY=win[!movingWindow!]H"
-			) else if !tempY! geq !sys.modeH! set /a "tempY=win[!movingWindow!]Y=sys.modeH-1, win[!movingWindow!]BY=win[!movingWindow!]Y+win[!movingWindow!]H-1"
-			>&3 echo=¤MW	!movingWindow!	X=!tempX!	Y=!tempY!
-			set temps=
-		) else if "!sys.click!" neq "0" (
-			if "!sys.mouseXpos!" neq "!sys.mouseXold!" if !sys.mouseXpos! geq 1 if !sys.mouseXpos! leq !sys.modeW! (
-				set /a ioTotal+=1
-				echo=mouseXpos=!sys.mouseXpos!
-			)
-			if "!sys.mouseYpos!" neq "!sys.mouseYold!" if !sys.mouseYpos! geq 1 if !sys.mouseYpos! leq !sys.modeH! (
-				set /a ioTotal+=1
-				echo=mouseYpos=!sys.mouseYpos!
-			)
-		)
-	)
-	set "sys.keys= !keysPressed!"
-	set "sys.keysRN=!sys.keys:-= !"
-	if "!sys.keysRN!" neq "!keysPressedOld!" (
-		echo=keysPressed=!sys.keys!
-		set /a ioTotal+=1
-	)
-	for %%k in (!keysPressedOld!) do set "sys.keysRN=!sys.keysRN: %%k = !"
-	if defined focusedWindow (
-		if "!sys.keysRN: =!" neq "" (
-			echo=keysPressedRN=!sys.keysRN!
-			set /a ioTotal+=1
-		)
-	)
 	set "keysPressedOld=!sys.keys:-= !"
 	
 	if "!sys.keys:~0,8!"==" -17-18-" if defined sys.keysRN (
@@ -143,9 +60,156 @@ for /l %%# in () do (
 			call :createProcess 0	systemb-devutil.bat
 		)
 	)
+	set "sys.clickOld=!sys.click!"
+	set "sys.click=!click!"
+	if "!sys.click!" neq "!sys.clickOld!" (
+		set sys.mouseXpos=!mouseXpos!
+		set sys.mouseYpos=!mouseYpos!
+		if !sys.mouseXpos! geq 1 if !sys.mouseXpos! leq !sys.modeW! if !sys.mouseYpos! geq 1 if !sys.mouseYpos! leq !sys.modeH! (
+			if "!sys.click!" neq "0" (
+				set "_focusedWindow=!focusedWindow!"
+				set focusedWindow=
+				for %%w in (!windows!) do if not defined focusedWindow (
+					if !sys.mouseYpos! geq !win[%%~w]Y! if !sys.mouseYpos! leq !win[%%~w]BY! if !sys.mouseXpos! geq !win[%%~w]X! if !sys.mouseXpos! leq !win[%%~w]BX! (
+						if "%%~w" neq "!_focusedWindow!" (
+							set "windows= %%w!windows: %%w = !"
+							echo=focusedWindow=%%~w
+							>&3 echo=¤FOCUS	%%~w
+							set /a ioTotal+=1
+						)
+						set "focusedWindow=%%~w"
+						if "!sys.keys!" neq "!sys.keys:-18-=!" (
+							if "!win[%%~w]D:~0,1!" neq "1" if "!sys.click!"=="1" (
+								set "movingWindow=!focusedWindow!"
+								set /a "movingWindowOffsetX=sys.mouseXpos-!win[%%~w]X!, movingWindowOffsetY=sys.mouseYpos-!win[%%~w]Y!, tempX=!win[%%~w]X!, tempY=!win[%%~w]Y!"
+							)
+						) else if "!sys.mouseYpos!"=="!win[%%~w]Y!" (
+							if "!mouseXpos!;!win[%%~w]D:~2,1!"=="!win[%%~w]X!;1" (
+								set /a ioTotal+=1
+								set "resizingWindow=!focusedWindow!"
+								echo=resizingWindow=!focusedWindow!
+								set originBY=true
+								set originBX=true
+							) else if "!win[%%~w]D:~0,1!" neq "1" (
+								set /a "movingWindowOffsetX=sys.mouseXpos-!win[%%~w]X!, tempX=!win[%%~w]X!, tempY=!win[%%~w]Y!"
+								set "movingWindow=!focusedWindow!"
+							)
+						) else if "!win[%%~w]D:~2,1!"=="1" if "!mouseYpos!"=="!win[%%~w]BY!" (
+							set /a ioTotal+=1
+							echo=resizingWindow=!focusedWindow!
+							set "resizingWindow=!focusedWindow!"
+							set originY=true
+							if "!mouseXpos!"=="!win[%%~w]BX!" (
+								set originX=true
+							) else if "!mouseXpos!"=="!win[%%~w]X!" (
+								set originBX=true
+							)
+						) else if "!mouseXpos!"=="!win[%%~w]BX!" (
+							set /a ioTotal+=1
+							echo=resizingWindow=!focusedWindow!
+							set "resizingWindow=!focusedWindow!"
+							set originX=true
+						) else if "!mouseXpos!"=="!win[%%~w]X!" (
+							set /a ioTotal+=1
+							echo=resizingWindow=!focusedWindow!
+							set "resizingWindow=!focusedWindow!"
+							set originBX=true
+						)
+					)
+				)
+				if defined _focusedWindow (
+					if not defined focusedWindow (
+						echo=focusedWindow=
+						set /a ioTotal+=4
+					) else set /a ioTotal+=3
+				) else set /a ioTotal+=3
+			) else (
+				if defined movingWindow (
+					set /a ioTotal+=5
+					if not defined tempX call :kernelPanic "Fatal error in kernel-side window manager" "Attempted to move a window to an undefined position. This is a critical bug, please report it at: github.com/Shivter14/Shivtanium/issues"
+					echo=win[!movingWindow!]X=!tempX!
+					echo=win[!movingWindow!]Y=!tempY!
+					set movingWindow=& set tempX=& set tempY=& set movingWindowOffsetX=& set movingWindowOffsetY=
+				) else if defined resizingWindow (
+					set /a ioTotal+=8
+					echo=win[!resizingWindow!]X=!tempX!
+					echo=win[!resizingWindow!]Y=!tempY!
+					echo=win[!resizingWindow!]W=!tempW!
+					echo=win[!resizingWindow!]H=!tempH!
+					echo=resizingWindow=
+					set resizingWindow=
+					for /f "delims==" %%a in ('set temp 2^>nul ^& set origin 2^>nul') do set "%%a="
+				) else set /a ioTotal+=3
+			)
+			echo=mouseXpos=!sys.mouseXpos!
+			echo=mouseYpos=!sys.mouseYpos!
+			if "!sys.keys!;!resizingWindow!"=="!sys.keys:-18-=!;" (
+				echo=click=!sys.click!
+			) else set /a ioTotal-=1
+		)
+	)
+	set sys.mouseXold=!sys.mouseXpos!
+	set sys.mouseYold=!sys.mouseYpos!
+	if "!random:~0,1!"=="1" (
+		set sys.mouseXpos=!mouseXpos!
+		set sys.mouseYpos=!mouseYpos!
+		if "!sys.mouseXold!;!sys.mouseYold!" neq "!sys.mouseXpos!;!sys.mouseYpos!" if defined movingWindow (
+			set /a "tempX=win[!movingWindow!]X=sys.mouseXpos-movingWindowOffsetX, temps=win[!movingWindow!]BX=win[!movingWindow!]X+win[!movingWindow!]W-1", "tempY=win[!movingWindow!]Y=sys.mouseYpos-movingWindowOffsetY, win[!movingWindow!]BY=win[!movingWindow!]Y+win[!movingWindow!]H-1"
+			if !tempX! lss 1 (
+				set /a "tempX=win[!movingWindow!]X=1, win[!movingWindow!]BX=win[!movingWindow!]W"
+			) else if !temps! gtr !sys.modeW! set /a "tempX=win[!movingWindow!]X=sys.modeW-win[!movingWindow!]W+1, win[!movingWindow!]BX=sys.modeW"
+			if !tempY! lss 1 (
+				set /a "tempY=win[!movingWindow!]Y=1, win[!movingWindow!]BY=win[!movingWindow!]H"
+			) else if !tempY! geq !sys.modeH! set /a "tempY=win[!movingWindow!]Y=sys.modeH-1, win[!movingWindow!]BY=win[!movingWindow!]Y+win[!movingWindow!]H-1"
+			>&3 echo=¤MW	!movingWindow!	X=!tempX!	Y=!tempY!
+			set temps=
+		) else if defined resizingWindow (
+			if "!sys.mouseXold!" neq "!sys.mouseXpos!" (
+				if defined originX (
+					set /a "x=sys.mouseXpos, low=tempMX=win[!resizingWindow!]X+win[!resizingWindow!]mW-1, temp.mouseXpos=%clamp:high=sys.modeW%, win[!resizingWindow!]W=tempW=(tempBX=win[!resizingWindow!]BX=temp.mouseXpos)-(tempX=win[!resizingWindow!]X)+1, tempY=win[!resizingWindow!]Y, tempBY=win[!resizingWindow!]BY, tempH=win[!resizingWindow!]H"
+				) else if defined originBX (
+					set /a "x=sys.mouseXpos, tempMX=win[!resizingWindow!]BX-win[!resizingWindow!]mW+1, low=1, temp.mouseXpos=%clamp:high=tempMX%, win[!resizingWindow!]W=tempW=(tempBX=win[!resizingWindow!]BX)-(tempX=win[!resizingWindow!]X=temp.mouseXpos)+1, tempY=win[!resizingWindow!]Y, tempBY=win[!resizingWindow!]BY, tempH=win[!resizingWindow!]H"
+				)
+			)
+			if "!sys.mouseYold!" neq "!sys.mouseYpos!" (
+				if defined originY (
+					set /a "x=sys.mouseYpos, low=tempMY=win[!resizingWindow!]Y+win[!resizingWindow!]mH-1, temp.mouseYpos=%clamp:high=(sys.modeH-1)%, tempX=win[!resizingWindow!]X, tempBX=win[!resizingWindow!]BX, tempW=win[!resizingWindow!]W, tempH=win[!resizingWindow!]H=(tempBY=win[!resizingWindow!]BY=temp.mouseYpos)-(tempY=win[!resizingWindow!]Y)+1"
+				) else if defined originBY (
+					set /a "x=sys.mouseYpos, tempMY=win[!resizingWindow!]BY-win[!resizingWindow!]mH+1, low=1, temp.mouseYpos=%clamp:high=tempMY%, tempX=win[!resizingWindow!]X, tempBX=win[!resizingWindow!]BX, tempW=win[!resizingWindow!]W, tempH=win[!resizingWindow!]H=(tempBY=win[!resizingWindow!]BY)-(tempY=win[!resizingWindow!]Y=temp.mouseYpos)+1"
+				)
+			)
+			set x=& set low=
+			if "!sys.mouseXold!;!sys.mouseYold!" neq "!sys.mouseXpos!;!sys.mouseYpos!" (
+				echo=¤CW	!resizingWindow!	!tempX!	!tempY!	!tempW!	!tempH!	^^!win[!resizingWindow!]title:~1^^!	^^!win[!resizingWindow!]theme^^!
+				echo=¤CTRL	DUMP	mainbuffer=^^!dwm.scene^^!	nul
+			)>&3
+		) else if "!sys.click!" neq "0" (
+			if "!sys.mouseXpos!" neq "!sys.mouseXold!" if !sys.mouseXpos! geq 1 if !sys.mouseXpos! leq !sys.modeW! (
+				set /a ioTotal+=1
+				echo=mouseXpos=!sys.mouseXpos!
+			)
+			if "!sys.mouseYpos!" neq "!sys.mouseYold!" if !sys.mouseYpos! geq 1 if !sys.mouseYpos! leq !sys.modeH! (
+				set /a ioTotal+=1
+				echo=mouseYpos=!sys.mouseYpos!
+			)
+		)
+	)
+	set "sys.keys= !keysPressed!"
+	set "sys.keysRN=!sys.keys:-= !"
+	if "!sys.keysRN!" neq "!keysPressedOld!" (
+		echo=keysPressed=!sys.keys!
+		set /a ioTotal+=1
+	)
+	for %%k in (!keysPressedOld!) do set "sys.keysRN=!sys.keysRN: %%k = !"
+	if defined focusedWindow (
+		if "!sys.keysRN: =!" neq "" (
+			echo=keysPressedRN=!sys.keysRN!
+			set /a ioTotal+=1
+		)
+	)
 
 	set /p io=&&for /f "tokens=1* delims=	" %%0 in ("!io!") do if "%%~0"=="registerWindow" (
-		for /f "tokens=1-7" %%a in ("%%~1") do (
+		for /f "tokens=1-9 delims=	" %%a in ("!io:~15!") do (
 			set "temp.id=%%~b"
 			set "temp.id=!temp.id:,=!"
 			set "temp.id=!temp.id:+=!"
@@ -171,6 +235,16 @@ for /l %%# in () do (
 			set "win[!temp.id!]W=%%~e"
 			set "win[!temp.id!]H=%%~f"
 			set "win[!temp.id!]D=!temp.d!"
+			if "!temp.d:~2,1!"=="1" if "%%~h"=="" (
+				set "win[!temp.id!]mW=%%~e"
+				set "win[!temp.id!]mH=%%~f"
+			) else if "%%~i"=="" (
+				set "win[!temp.id!]mW=%%~h"
+				set "win[!temp.id!]mH=%%~f"
+			) else (
+				set "win[!temp.id!]mW=%%~h"
+				set "win[!temp.id!]mH=%%~i"
+			)
 			set /a "win[!temp.id!]X=win[!temp.id!]X, win[!temp.id!]Y=win[!temp.id!]Y, win[!temp.id!]W=win[!temp.id!]W, win[!temp.id!]H=win[!temp.id!]H, win[!temp.id!]BX=win[!temp.id!]X+win[!temp.id!]W-1, win[!temp.id!]BY=win[!temp.id!]Y+win[!temp.id!]H-1", ioTotal+=1
 			set "focusedWindow=!temp.id!"
 			echo=focusedWindow=!temp.id!
@@ -188,7 +262,9 @@ for /l %%# in () do (
 			echo=focusedWindow=
 			set focusedWindow=
 		)
+		for /f "delims==" %%a in ('set temp 2^>nul ^& set origin 2^>nul') do set "%%a="
 		if "%%~1"=="!movingWindow!" set movingWindow=
+		if "%%~1"=="!resizingWindow!" set resizingWindow=
 	) else if "%%~0"=="exitProcess" (
 		set "PID=%%~1"
 		set /a PID=PID
@@ -208,7 +284,9 @@ for /l %%# in () do (
 				set focusedWindow=
 				for %%w in (!windows!) do if not defined focusedWindow set "focusedWindow=%%~w"
 			)
+			for /f "delims==" %%a in ('set temp 2^>nul ^& set origin 2^>nul') do set "%%a="
 			if "%%~w"=="!movingWindow!" set movingWindow=
+			if "%%~w"=="!resizingWindow!" set resizingWindow=
 			for /f "tokens=1 delims==" %%a in ('set "win[%%~w]" 2^>nul') do set "%%a="
 		)
 		set /a ioTotal+=2
@@ -224,7 +302,7 @@ for /l %%# in () do (
 		set /a ioTotal+=1
 		echo=windowsT=!windowsT!
 	) else if "%%~0"=="config" (
-		for /f "tokens=1* delims==^!" %%x in ("%%~1") do for %%s in (
+		for /f "tokens=1* delims==^!" %%x in ("!io:~7!") do for %%s in (
 			lowPerformanceMode
 			reduceMotion
 		) do if "%%x"=="%%s" (
@@ -238,7 +316,7 @@ for /l %%# in () do (
 			) >&3
 		)
 	) else if "%%~0"=="minimizeWindow" (
-		for /f "tokens=1*" %%1 in ("%%~1") do (
+		for /f "tokens=1* delims=	" %%1 in ("!io:~15!") do (
 			if not defined win[%%~2] call :kernelPanic "Function minimizeWindow failed" "Tried to minimize a non-existent window.\nProcess: %%~1\nWindow: %%~2"
 			set "windows=!windows: "%%~2"=!"
 			if "!focusedWindow!"=="%%~2" (
@@ -251,34 +329,37 @@ for /l %%# in () do (
 			if "!movingWindow!"=="%%~2" set movingWindow=
 			if defined win[%%~2]off call :kernelPanic "Function minimizeWindow failed" "Tried to minimize a window, which was already minimized.\nProcess: %%~1\nWindow: %%~2"
 			if not exist "!sst.dir!\temp\DWM-offload" md "!sst.dir!\temp\DWM-offload" || call :kernelPanic "Function minimizeWindow failed" "Failed to create directory: ~\temp\DWM-offload\nProcess: %%~1\nWindow: %%~2"
-			if exist "!sst.dir!\temp\DWM-offload\snapshot.%%~1.%%~2" call :kernelPanic "Function minimizeWindow failed" "Window is already minimized"
+			if exist "!sst.dir!\temp\DWM-offload\snapshot.%%~2" call :kernelPanic "Function minimizeWindow failed" "Window is already minimized"
 			(
-				echo=¤CTRL	DUMP	win[%%~2]	"!sst.dir!\temp\DWM-offload\snapshot.%%~1.%%~2"
+				echo=¤CTRL	DUMP	win[%%~2]	"!sst.dir!\temp\DWM-offload\snapshot.%%~2"
 				echo=¤DW	%%~2
 			) >&3
 			set "win[%%~2]off=1"
 		)
 	) else if "%%~0"=="restoreWindow" (
-		for /f "tokens=1*" %%1 in ("%%~1") do (
+		for /f "tokens=1* delims=	" %%1 in ("!io:~14!") do (
 			if not defined win[%%~2] call :kernelPanic "Function restoreWindow failed" "Tried to restore a non-existent window.\nProcess: %%~1\nWindow: %%~2"
-			if not defined win[%%~2]off call :kernelPanic "Function restoreWindow failed" "Tried to restore a non-minimized window.\nProcess: %%~1\nWindow: %%~2"
 			set "focusedWindow=%%~2"
-			if not exist "!sst.dir!\temp\DWM-offload" call :kernelPanic "Function restoreWindow failed" "DWM offload memory was lost.\nProcess: %%~1\nWindow: %%~2"
-			if not exist "!sst.dir!\temp\DWM-offload\snapshot.%%~1.%%~2" call :kernelPanic "Function restoreWindow failed" "DWM offload memory for the window being currently restored was lost.\nProcess: %%~1\nWindow: %%~2"
+			if defined win[%%~2]off (
+				if not exist "!sst.dir!\temp\DWM-offload" call :kernelPanic "Function restoreWindow failed" "DWM offload memory was lost.\nProcess: %%~1\nWindow: %%~2"
+				if not exist "!sst.dir!\temp\DWM-offload\snapshot.%%~2" call :kernelPanic "Function restoreWindow failed" "DWM offload memory for the window being currently restored was lost.\nProcess: %%~1\nWindow: %%~2"
+				echo=win[%%~2]X=!win[%%~2]X!
+				echo=win[%%~2]Y=!win[%%~2]Y!
+				set /a ioTotal+=3
+			) else set /a ioTotal+=1
 			(
-				echo=¤CTRL	LOAD	"!sst.dir!\temp\DWM-offload\snapshot.%%~1.%%~2"	/deleteAfterLoad
-				echo=¤MW	%%~2	x=!win[%%~2]X!	y=!win[%%~2]Y!
+				if defined win[%%~2]off (
+					echo=¤CTRL	LOAD	"!sst.dir!\temp\DWM-offload\snapshot.%%~2"	/deleteAfterLoad
+					echo=¤MW	%%~2	x=!win[%%~2]X!	y=!win[%%~2]Y!
+				)
 				echo=¤FOCUS	%%~2
 			) >&3
 			set "windows= "%%~2"!windows: "%%~2"=!"
-			set /a ioTotal+=3
 			echo=focusedWindow=!focusedWindow!
-			echo=win[%%~2]X=!win[%%~2]X!
-			echo=win[%%~2]Y=!win[%%~2]Y!
 			set "win[%%~2]off="
 		)
 	) else if "%%~0"=="modifyWindowProperties" (
-		for /f "tokens=1-6" %%b in ("%%~1") do (
+		for /f "tokens=1-6" %%b in ("!io:~23!") do (
 			set "temp.id=%%~b"
 			set "temp.id=!temp.id:,=!"
 			set "temp.id=!temp.id:+=!"
@@ -309,7 +390,10 @@ for /l %%# in () do (
 		) else if /I "%%~1"=="fastReboot" (
 			call :shutdown 13
 		)
+	) else (
+		>&2 echo=[!date! !time!] Invalid kernel call: !io!
 	)
+)
 )
 :createProcess
 set PID=!random!
@@ -356,7 +440,9 @@ if "!processes: =!"=="" (
 		echo=focusedWindow=!focusedWindow!
 		>&3 echo=¤FOCUS	!focusedWindow!
 	)
+	for /f "delims==" %%a in ('set temp 2^>nul ^& set origin 2^>nul') do set "%%a="
 	if "%%~w"=="!movingWindow!" set movingWindow=
+	if "%%~w"=="!resizingWindow!" set resizingWindow=
 	for /f "tokens=1 delims==" %%a in ('set "win[%%~w]" 2^>nul') do set "%%a="
 )
 if exist "temp\proc\PID-!PID!" del "temp\proc\PID-!PID!" >nul 2>&1 <nul
@@ -423,7 +509,6 @@ set "cosr=!cosr: =!"
 set "sin=(a=((x*31416/180)%%62832)+(((x*31416/180)%%62832)>>31&62832), b=(a-15708^a-47124)>>31,a=(-a&b)+(a&~b)+(31416&b)+(-62832&(47123-a>>31)),a-a*a/1875*a/320000+a*a/1875*a/15625*a/16000*a/2560000-a*a/1875*a/15360*a/15625*a/15625*a/16000*a/44800000) / 10000"
 set "cos=(a=((15708-x*31416/180)%%62832)+(((15708-x*31416/180)%%62832)>>31&62832), b=(a-15708^a-47124)>>31,a=(-a&b)+(a&~b)+(31416&b)+(-62832&(47123-a>>31)),a-a*a/1875*a/320000+a*a/1875*a/15625*a/16000*a/2560000-a*a/1875*a/15360*a/15625*a/15625*a/16000*a/44800000) / 10000"
 set /a "PI=(35500000/113+5)/10, HALF_PI=(35500000/113/2+5)/10, TAU=TWO_PI=2*PI, PI32=PI+HALF_PI"
-title !dmp.dwm.aero!
 if exist "!sst.boot.dir!" (
 	if "%~1"=="27" copy nul "!sst.boot.dir!\befiReboot.cww" > nul
 	if "%~1"=="13" >"!sst.boot.dir!\befiReboot.cww" echo 13
