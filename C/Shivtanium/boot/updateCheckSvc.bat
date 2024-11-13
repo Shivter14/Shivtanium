a3917e367ef3853de2b3b38fff6a0eb7c217cf0d
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-11-13 06:56:56 -0800

    Finished the update check service

--- C/Shivtanium/boot/updateCheckSvc.bat
+++ C/Shivtanium/boot/updateCheckSvc.bat
@@ -8,6 +8,7 @@ if /I "!sys.boot.disableUpdateChecks!" neq "True" (
 	set latest.tag=
 	set latest.ver=
 	set latest.subvinfo=
+	set latest.downloadLink=
 	for /f "delims=*^!" %%a in ('curl -Lsm 3 "https://raw.githubusercontent.com/Shivter14/Shivtanium/main/update.dat"') do (
 		set "latest.%%a"
 	)
@@ -15,15 +16,18 @@ if /I "!sys.boot.disableUpdateChecks!" neq "True" (
 	if defined latest.ver if "!latest.ver!" neq "!sys.ver!" goto updateFound
 	if defined latest.subvinfo if "!latest.subvinfo!" neq "!sys.subvinfo!" goto updateFound
 )
-
 copy nul "!sst.dir!\temp\pf-%~n0" > nul
 exit /b 0
 :updateFound
+if not defined latest.downloadLink goto nevermindUpdateNotFound
 (
 	echo=!latest.tag!
 	echo=!latest.ver!
 	echo=!latest.subvinfo!
+	echo=!latest.downloadLink!
 ) > "!sst.dir!\temp\update.dat"
+if "%~1"=="/q" goto nevermindUpdateNotFound
 >>"!sst.dir!\temp\kernelPipe" echo=createProcess	0	boot\update.bat
+:nevermindUpdateNotFound
 copy nul "!sst.dir!\temp\pf-%~n0" > nul
 exit /b 0
